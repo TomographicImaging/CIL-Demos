3c9fbdd977f0145498ebe4fc2f9c2a1c1bc87a7f
Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
2024-10-31 15:35:47 +0000

    Add more detail on geometry set-up to where's my reader (#200)
    * Add info on axes for geometry definition
    
    * Add explanation on deviating from defaults

--- demos/1_Introduction/exercises/snippets/03_exC.py
+++ demos/1_Introduction/exercises/snippets/03_exC.py
@@ -1,14 +1,16 @@
 # create the geometry using the `Cone2D` convenience method.
-# These are relative positions of each so you may have set it up differently to this answer but it doesn't make it wrong.
+# We recommend defining the source_to_detector_distance and source_to_object_distance along the y axis, as this is our default setup in CIL.
+# The positions of the soruce, detector and rotation axis are relative, so you may have set it up differently to this answer but it doesn't make it wrong.
+
 geometry = AcquisitionGeometry.create_Cone2D(source_position=[0,0],
-                                             detector_position=[0,source_to_detector_distance],
-                                             rotation_axis_position=[0,source_to_object_distance])
+                                             detector_position=[0, source_to_detector_distance], # note we have defined this along the y axis
+                                             rotation_axis_position=[0, source_to_object_distance]) # note we have defined this along the y axis
 
 # set the angles, remembering to specify the units
 geometry.set_angles(angles, angle_unit='degree')
 
 # set the detector shape and size
-geometry.set_panel(number_of_pixels,pixel_size)
+geometry.set_panel(number_of_pixels, pixel_size)
 
 # set the order of the data
 geometry.set_labels(axis_labels)
