1ccafd3f173fdfafe61b59089e8232b40f0cdc9a
Gemma Fardell <gemma.fardell@stfc.ac.uk>
2026-06-19 16:47:03 +0000

    bugfix: updated euler angles

--- misc/tigre_tilted_parallel.py
+++ misc/tigre_tilted_parallel.py
@@ -16,13 +16,13 @@ from cil.framework import ImageData, ImageGeometry, AcquisitionGeometry
 from cil.plugins.tigre import ProjectionOperator
 # %% Create phantom
 lines = np.zeros((6, 16,16), dtype=np.float32)
-lines[2:4,2:14,2:6] = 1
+lines[2:4,2:14,2:6] = 0.5
 lines[2:4,2:14,10:14] = 1
 
 ig = ImageGeometry(lines.shape[1],lines.shape[2],lines.shape[0])
 lines = ImageData(lines, geometry=ig)
 show2D(lines,
-       slice_list=[(0, int(lines.shape[0]/2)), (1, int(lines.shape[1]/2)), (2, 3)], 
+       slice_list=[(0, int(lines.shape[0]/2)), (1, int(lines.shape[1]/2)), (2, 3)],
        num_cols=3)
 
 # %% Start with an untilted geometry
@@ -30,7 +30,7 @@ untilted_rotation_axis = np.array([0, 0, 1])
 angles = np.arange(0,360,1)
 ag_untilted = AcquisitionGeometry.create_Parallel3D(rotation_axis_direction=untilted_rotation_axis)\
     .set_angles(angles)\
-    .set_panel(lines.shape[1:3])
+    .set_panel(lines.shape[1:3],origin='top-left')
 show_geometry(ag_untilted)
 
 # %% Create projections
@@ -51,13 +51,12 @@ tilted_rotation_axis = rotation_matrix.apply(untilted_rotation_axis)
 angles = np.arange(0,360,1)
 ag = AcquisitionGeometry.create_Parallel3D(rotation_axis_direction=tilted_rotation_axis)\
     .set_angles(angles)\
-    .set_panel(lines.shape[1:3])
+    .set_panel(lines.shape[1:3],origin='top-left')
 show_geometry(ag)
 
 # %% Create the projections
 A = ProjectionOperator(ig, ag)
 
-plt.plot(A.tigre_geom.angles)
 proj = A.direct(lines) 
 
 show2D([proj.array[0], proj.array[90], proj.array[180]], 
@@ -68,7 +67,7 @@ show2D([proj.array[0], proj.array[90], proj.array[180]],
 # %% Check how it looks using tigre directly
 geo = tigre.geometry()
 geo.DSO = 5
-geo.DSD = 5
+geo.DSD = 5000 #move detector outside of object otherwise it clips
 geo.nDetector = np.array([16, 16])
 geo.dDetector = np.array([1, 1]) 
 geo.sDetector = geo.nDetector * geo.dDetector
@@ -81,18 +80,17 @@ geo.accuracy = 0.5
 # conversion to tigre angles
 angles_t = -(np.deg2rad(ag.angles)  + np.pi/2)
 
-euler_angles_old = []
-for angle in angles_t:
-    R1 = R.from_euler("z", angle, degrees=False)
-    combined = rotation_matrix * R1
-    euler = combined.as_euler("ZYZ", degrees=False)
-    euler_angles_old.append(euler)
+R_tilt = R.from_euler("Y", tilt_rad, degrees=False)
+euler_angles = []
 
+for i, theta in enumerate(angles_t):
+    R_rot = R.from_euler("Z",theta)
+    R_Full = R_rot * R_tilt
+    euler = R_Full.as_euler("ZYZ")
+    euler_angles.append(euler)
 
-plt.plot(euler_angles_old)
-
-euler_angles_old = np.array(euler_angles_old, dtype=np.float32) 
-out = tigre.Ax(lines.array.astype(np.float32), geo, euler_angles_old, "interpolated")
+euler_angles = np.array(euler_angles, dtype=np.float32) 
+out = tigre.Ax(lines.array, geo, euler_angles, "interpolated")
 
 show2D([out[0], out[90], out[180]], 
        ['0', r'$\pi/2$', r'$\pi$'],
